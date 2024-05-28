8903e07c8767132c66b742701aa2ee7f0a2bb976
Michael Berney <michaeljlb3@gmail.com>
2024-05-28 17:55:40 +0100

    Further development of mlflow code

--- ml_flow.py
+++ ml_flow.py
@@ -3,22 +3,50 @@
 2. Evaluation: To generate evaluation metrics for each model which is run.
 """
 
-# IMPORTS --------------------------------------------------------------------------------------------------------------
+# IMPORTS & SETUP ------------------------------------------------------------------------------------------------------
 
+import atexit
+import logging
 import subprocess
+
+logging.basicConfig(level=logging.INFO)
 
 # ML-FLOW FUNCTIONS ----------------------------------------------------------------------------------------------------
 
-import sys
-print(sys.executable)
 
-#command = ['python', '-m', 'mlflow', 'server', '--host', '127.0.0.1', '--port', '8080']
-command = ['python', '-m', 'pip', 'list']
+def _close_mlflow_server(server_process: object) -> None:
+    """Closes the supplied server process.
 
-try:
-    result = subprocess.run(command, capture_output=True, text=True, check=True)
-    print("Output:", result.stdout)
+    Args:
+        server_process (object): The server object in question.
+    """
+    server_process.terminate()
+    server_process.wait()
+    server_process.kill()
+    server_process.wait()
+    logging.info("MLFlow server terminated.")
 
-except subprocess.CalledProcessError as e:
-    print("Error:", e.stderr)
-    print("Return Code:", e.returncode)
+
+def mlflow_server(port: int = 8080) -> subprocess.Popen:
+    """Uses command line code to start an ML-Flow server at the port.
+
+    Args:
+        port (int, optional): The port at which to run the server. Defaults to 8080.
+    
+    Returns:
+        server_process (subprocess.Popen): The Popen object representing the server. 
+    """
+    server_process = None
+    command = ['python', '-m', 'mlflow', 'server', '--host', '127.0.0.1', '--port', str(port)]
+
+    try:
+        server_process = subprocess.Popen(command)
+        logging.info('Successfully running ML-Flow server. The server will terminate at the end of runtime.')
+        atexit.register(_close_mlflow_server, server_process)
+
+    except subprocess.CalledProcessError as e:
+        logging.warning('Unable to run ML-Flow server.')
+        print("Error:", e.stderr)
+        print("Return Code:", e.returncode)
+
+    return server_process
