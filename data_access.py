d78738ef169dfb418f342dc049257f9d96ea88ad
fahima-a <fahima.ahm@outlook.com>
2024-05-28 13:45:43 +0100

    fahima edits

--- data_access.py
+++ data_access.py
@@ -15,26 +15,35 @@ def download_files_10k(ticker: str, dest_folder: str) -> None:
     Returns:
         None
     """
+
+    # Reads the CSV containing the list of all the tickers, their CIK (Central Index Key, unique identifier) and 
+    # CIK Zeros (CIK with padding zeros at the beginning if needed, used to construct URLs for API requests, SEC EDGAR system requires CIK numbers to be 10 digits)
     SP100data = pd.read_csv("data\companyData.csv", index_col=False)
     SP100data = SP100data.set_index("ticker")
     SP100data["CIK Zeros"] = SP100data["CIK Zeros"].astype(str).str.zfill(10)
 
+    # Identifying ticker name, its CIK and CIK Zeros 
     ticker = ticker
     cik = SP100data.loc[ticker]["CIK"]
     cik_zero = SP100data.loc[ticker]["CIK Zeros"]
 
+    # Need to specify who is requesting the data, usually an email address so SEC can get into contact if there are issues or changes
     headers = {"User-Agent": "fahimaahmed@kubrickgroup.com"}
+    
+    # Get the json files for the company using requests, these files contain information about the reports
     company_files = requests.get(f"https://data.sec.gov/submissions/CIK{cik_zero}.json", headers=headers)
 
+    # Dataframe of all the recent filings, mask to get all the 10 K files
     allForms = pd.DataFrame.from_dict(company_files.json()["filings"]["recent"])
-
     mask = allForms["form"] == "10-K"
     forms_10k = allForms[mask]
     forms_10k = forms_10k[["accessionNumber", "reportDate", "form", "primaryDocument"]].reset_index(drop=True)
 
+    # Create destination folder if it does not already exist
     if not os.path.exists(dest_folder):
         os.mkdir(dest_folder)
-
+    
+    # Loop through all the 10 K files, get the required information to construct the URL, extracted using requests and saved at html files in desired folder
     for i in range(0, len(list(forms_10k["primaryDocument"]))):
         doc_link = forms_10k["primaryDocument"][i]
         accessionNumber = forms_10k["accessionNumber"][i]
